1c335bb5958b36897ccd28d40adec02442021629
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-05 13:52:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write($"Enter a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of the digits of the number {x} is: {SumDigits(x)}");
+
+int SumDigits(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        int digit = x % 10;
+        sum += digit;
+        x /= 10;
+    }
+    return sum;
+}
